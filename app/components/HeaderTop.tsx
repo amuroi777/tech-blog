ea170bfcda9f1a6330ab82884f02509740e75df4
amuroi777 <muroiaki7722@gmail.com>
2024-09-07 12:48:12 +0900

    header動作の修正

--- app/components/HeaderTop.tsx
+++ app/components/HeaderTop.tsx
@@ -1,8 +1,8 @@
 import { Box, Flex, HStack, IconButton, useDisclosure, Stack, Link, Button, Drawer, DrawerBody, DrawerHeader, DrawerOverlay, DrawerContent, DrawerCloseButton, Image, Text } from "@chakra-ui/react";
 import { HamburgerIcon, EditIcon } from "@chakra-ui/icons";
 import {getAuth, signOut} from "firebase/auth";
-import { useAuthContext } from "../context/AuthContext";
-import {useRouter} from 'next/navigation'
+import { AuthProvider, useAuthContext } from "../context/AuthContext";
+import { useRouter, usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react';
 import { AuthGuard } from "../authguard/AuthGuard";
 
@@ -13,7 +13,14 @@ const HeaderTop = () => {
   const [logoutSuccess, setLogoutSuccess] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false); 
   const [showUserMenu, setShowUserMenu] = useState(false);
+  const [isAddPage, setIsAddPage] = useState(false);
 
+    const handlePublish = () => {
+    router.push('/add?publish=true'); // クエリパラメータで 'publish' を設定
+  };
+
+  // 現在いるページのパスを取得
+  const pathname = usePathname();
 
   // 現在ログインしているユーザーを取得する
   const currentUser = authContext ? authContext.user: null;
@@ -26,6 +33,11 @@ const HeaderTop = () => {
       setLoggedIn(false);
     }
   }, [currentUser]);
+
+  // ページ遷移時に/addページかどうか監視
+  useEffect(() => {
+    setIsAddPage(window.location.pathname === '/add');
+  }, [router])
 
   const logout = () =>{
     const auth = getAuth();
@@ -44,7 +56,7 @@ const HeaderTop = () => {
     <AuthGuard>
       <Box bg="gray.300" px={4}>
         <Flex h={20} alignItems="center" pl="20px" justifyContent="space-between">
-          <Box fontSize="4xl" fontWeight="bold" color="blackAlpha.400">
+          <Box fontSize="4xl" fontWeight="bold" color="blackAlpha.400" onClick={() => router.push('/')} cursor="pointer">
             Logo
           </Box>
           <HStack spacing={8} alignItems="center">
@@ -64,19 +76,27 @@ const HeaderTop = () => {
                   Home
                 </Link>
               </Button>
+              
               <Button
                 mb={12}
                 rounded="60px"
                 padding="18px"
                 mt="50px"
-                bg={{ base: "blue.400", md: "blackAlpha.800" }}
+                bg={isAddPage ? "blue.400" : "blackAlpha.800"}
                 width="60%"
                 fontSize={{ base: "md", md: "md", lg: "md" }}
-                color={{ base: "black", sm: "white" }}
+                color={isAddPage ? "#000" : "#fff"}
+                onClick={() => {
+                  if(isAddPage) {
+                    handlePublish()
+                  } else {
+                    router.push('/add')
+                  }
+                }}
               >
               <Link px={2} py={1} rounded="md" _hover={{ textDecoration: "none" }} href="#">
-                <EditIcon mr={2} />
-                Create
+                {isAddPage ? null : <EditIcon mr={2} />}
+                {isAddPage ? "Publish" : "Create"}
               </Link>
             </Button>
               
@@ -92,12 +112,31 @@ const HeaderTop = () => {
                 />
                 {showUserMenu && (
                   <Box position="absolute" top="70px" right="20px" bg="gray.400" p={4} borderRadius="md" shadow="md">
-                    <Text mb={2} color="#000">
+                    <Flex direction="column" align="center">
+                    <Text mb={4} color="#000">
                       {currentUser?.displayName || 'User name'} {/* ユーザー名を表示 */}
                     </Text>
-                    <Button bg="red.400" size="sm" rounded="60px" padding="15px" onClick={logout}>
+                    <Button
+                      bg="blue.400"
+                      size= "sm"
+                      rounded="60px"
+                      padding="15px"
+                      mb={4}
+                      onClick={() => router.push('/profile')}
+                      >
+                      Profile
+                    </Button>
+
+                    <Button
+                      bg="red.400"
+                      size="sm"
+                      rounded="60px"
+                      padding="15px"
+                      onClick={logout}
+                    >
                       Log out
                     </Button>
+                    </Flex>
                   </Box>
                 )}
               </>
@@ -120,7 +159,6 @@ const HeaderTop = () => {
                   </Link>
                 </Button>
               )}
-              
             </HStack>
 
             <Button
@@ -128,15 +166,22 @@ const HeaderTop = () => {
               rounded="60px"
               padding="5px"
               mt="50px"
-              bg="blackAlpha.800"
+              bg={isAddPage ? "blue.400" : "blackAlpha.800"}
               width={{ base: "60%", sm: "60%", md: "60%", lg: "40%" }}
               fontSize={{ base: "md", md: "md", lg: "md" }}
               color="#fff"
               display={{ base: "block", md: "none" }}
+              onClick={() => {
+                if(isAddPage) {
+                  handlePublish()
+                } else {
+                  router.push('/add')
+                }
+              }}
             >
             <Link px={2} py={1} rounded="md" _hover={{ textDecoration: "none" }} href="#">
-              <EditIcon mr={2} />
-              Create
+              {isAddPage ? null : <EditIcon mr={2} />}
+              {isAddPage ? "Publish" : "Create"}
             </Link>
           </Button>
             <IconButton size="lg" bg="none" color="#000" icon={<HamburgerIcon boxSize={8} />} aria-label="Open Menu" display={{ md: "none" }} onClick={onOpen} />
@@ -150,8 +195,24 @@ const HeaderTop = () => {
               <DrawerBody bg="gray.300">
                 <Stack as="nav" spacing={4} alignItems="center">
                 {loggedIn ? (
+                  <>
+                  <Button
+                    mt="75px"
+                    pl="25px"
+                    rounded="60px"
+                    padding="5px"
+                    border="1px solid"
+                    borderColor="#000"
+                    bg="none"
+                    width="50%"
+                    fontSize="sm"
+                    display={{ base: "block", md: "none" }}
+                    onClick={() => router.push(`/profile`)}
+                  >
+                    <Text color="#000">Profile</Text>
+                  </Button>
                     <Button
-                      mt="75px"
+                      mt="15px"
                       pl="25px"
                       rounded="60px"
                       padding="5px"
@@ -165,6 +226,7 @@ const HeaderTop = () => {
                     >
                       <Text color="#000">Log Out</Text>
                     </Button>
+                  </>
                   ) : (
                     <Button
                       mt="75px"
